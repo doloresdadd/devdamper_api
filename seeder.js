cf5989d9abff2b99aa34751c99ae28998223f862
Bethanie Innes <doloresdadd@gmail.com>
2020-10-17 16:39:48 +1100

    Add course model

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 //Load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 const asyncHandler = require("./middleware/async");
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -19,13 +20,18 @@ mongoose.connect(process.env.MONGO_URI, {
 
 //Read json files
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -37,6 +43,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data deleted...".red.inverse);
     process.exit();
   } catch (err) {
@@ -44,8 +51,8 @@ const deleteData = async () => {
   }
 };
 
-if (process.argv[2] === '-i') {
+if (process.argv[2] === "-i") {
   importData();
-} else if (process.argv[2] === '-d') {
+} else if (process.argv[2] === "-d") {
   deleteData();
 }
